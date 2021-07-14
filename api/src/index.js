30a6676dfa5411c0da9631fd4c726e983138dc28
danroy169 <danielwroy90@gmail.com>
2021-07-14 13:35:25 -0400

    stuff

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
+import sql from 'mssql'
 import { readFile } from 'node:fs/promises'
+import { config } from 'dotenv'
 
+
+config()
 
 const app = express()
 
@@ -44,6 +48,20 @@ app.get('/vehicle-sessions/month', (req, res) => {
         })
 })
 
+// app.get('/calibrations/year', (req, res) => {
+
+//     res.set({
+//         'Access-Control-Allow-Origin': '*',
+//         'Access-Control-Allow-Credentials': true
+//     })
+
+//     read('../../Calibrations_and_Scans.json')
+//         .then(obj => {
+//             const filtered = obj.filter(item => { return item.REPORTTYPE === 'CALIBRATIONREPORT' && item.YEAR === 2021 })
+//             res.json(filtered)
+//         })
+// })
+
 app.get('/calibrations/year', (req, res) => {
 
     res.set({
@@ -51,12 +69,30 @@ app.get('/calibrations/year', (req, res) => {
         'Access-Control-Allow-Credentials': true
     })
 
-    read('../../Calibrations_and_Scans.json')
-        .then(obj => {
-            const filtered = obj.filter(item => { return item.REPORTTYPE === 'CALIBRATIONREPORT' && item.YEAR === 2021 })
-            res.json(filtered)
-        })
+    sql.on('error', err => { res.send(err) })
+
+    const config = {
+        user: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        server: process.env.SERVER,
+        database: process.env.REPORT_DB,
+        options: {
+            trustServerCertificate: true // true for local dev / self-signed certs
+          }
+    }
+
+    sql.connect(config)
+    .then(() => {
+        return sql.query`SELECT  COUNT([REPORTTIMESTAMP]) AS ThisYear
+        FROM [DB_IAMReportServices_SQL].[RS].[REPORT_DATA_PROCESSED]
+        WHERE [REPORTTYPE] = 'CALIBRATIONREPORT'
+        AND [REPORTTIMESTAMP] between (select dateadd(year, -1, getdate())) and  getdate()
+        `
+    })
+    .then(result => { res.json(result) })
+    .catch(err => { res.send(err) })
 })
+
 
 app.get('/calibrations/month', (req, res) => {
 
